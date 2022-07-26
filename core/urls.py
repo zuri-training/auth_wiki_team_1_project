21b226bbd6ee61b7f4b515e6ec37eb41966e49d0
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-26 13:31:58 +0100

    add url

--- core/urls.py
+++ core/urls.py
@@ -18,13 +18,15 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
     path('api/auth/', include('auth.urls', namespace='auth'))
 ]
