33beb515c0032861f2eab195a35e7baaeb92adcc
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-26 13:31:06 +0100

    add user serializer

---
+++ auth/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "date_joined",
+            "last_login"
+        )
