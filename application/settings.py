b365291e1bfab62b39f9c45693bc6578054c5e73
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-26 10:07:00 +0100

    add rest permission configs

--- application/settings.py
+++ application/settings.py
@@ -52,6 +52,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
 ROOT_URLCONF = 'application.urls'
 
 TEMPLATES = [
