0aa659f7d2fe849a54ef0a33f562f0e8403ec954
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-26 10:07:24 +0100

    add authentication config

--- application/settings.py
+++ application/settings.py
@@ -55,7 +55,10 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 ROOT_URLCONF = 'application.urls'
