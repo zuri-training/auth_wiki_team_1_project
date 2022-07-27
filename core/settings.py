e270cf00685fe2e5f60ab9d42cda45ef1899fbb7
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-27 07:26:49 +0100

    add static config

--- core/settings.py
+++ core/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATIC_ROOT = '/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
