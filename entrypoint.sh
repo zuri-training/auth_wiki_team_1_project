786a22e589c5d76e93c85ac6af8b039bd22d207d
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-27 07:22:30 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/shell
 
 python manage.py migrate  --no-input
+python manage.py collectstatic --no-input
 
 gunicorn core.asgi:application --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
