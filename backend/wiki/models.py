3b5b07f81067d6ec876d500514eed709fcb38d63
jonathan <jonathan>
2022-07-28 07:26:12 -0700

    made a contribution to create the comment section

--- backend/wiki/models.py
+++ backend/wiki/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Comment(models.Model):
+    content= models.TextField()
+    user_id= models.ForeignKey(User, on_delete=models.CASCADE)
+    package_id= models.ForeignKey(Package, on_delete=models.CASCADE)
+    created_date= models.DateTimeField(auto_now_add=True)
+    updated_date= models.DateTimeField(auto_now=True)
+
+
