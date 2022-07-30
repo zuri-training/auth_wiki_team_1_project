a84c76c0f82b89ee6de726f967cbbdd0f4d05402
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-30 11:03:52 +0100

    update URIS

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -27,7 +27,7 @@ from .views import HomeView
 urlpatterns = [
     path("", HomeView.as_view()),
     path('admin/', admin.site.urls),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
