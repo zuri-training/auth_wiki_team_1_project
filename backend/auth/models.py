03d09ac5ec630a00470a01d68c62d35fa4d019e6
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-07-30 11:00:31 +0100

    move model to wiki models

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Package(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    documentation = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField()
