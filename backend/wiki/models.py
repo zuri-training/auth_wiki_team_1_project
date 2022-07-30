842626e54b452e7cc75bb7ba60124b5dd8a033ca
TimmyKwesi <timmysteinx@gmail.com>
2022-07-30 02:43:17 +0000

    Reaction Model added

--- backend/wiki/models.py
+++ backend/wiki/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,6 +17,13 @@ class File(models.Model):
     download_counts = models.PositiveIntegerField(default=0)
     created_date = models.DateTimeField()
     updated_date = models.DateTimeField()
+    
+class Reaction(models.Model): 
+    # Without the Post model, I had to use a # as placeholder for now.
+    post_id = "#"
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_date = models.DateField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='posts')
 
 
     def __str__(self):
