c0a5b26c273e00800a8251f0ccc3362b27617786
zeelyha <ngozi.priscillia29@gmail.com>
2022-08-01 01:23:54 +0800

    Created CRUD for Post model

--- backend/wiki/views.py
+++ backend/wiki/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostListApi(generics.ListAPIView):
+    queryset = Post.objects.filter(active= True)
+    serializer_class = PostSerializer
+
+
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Post.objects.filter(active=True)
+    serializer_class = PostSerializer
+
+
+class PostDetailApi(generics.RetrieveAPIView):
+    queryset = Post.objects.filter(active=True)
+    serializer_class = PostSerializer
+
+
+class PostUpdateApi(generics.UpdateAPIView):
+    queryset = Post.objects.filter(active=True)
+    serializer_class = PostSerializer
+
+
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset = Post.objects.filter(active=True)
+    serializer_class = PostSerializer
+
+ 
+
+
+
+
