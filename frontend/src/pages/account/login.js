ec098124b6282d4d5995c12d3b7e4c1b76c648f8
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-02 08:32:36 +0100

    Update login.js

--- frontend/src/pages/account/login.js
+++ frontend/src/pages/account/login.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { toast } from "react-toastify";
 import { ThreeDots } from "react-loader-spinner";
 import { login, reset_register_success } from "../../actions/account";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
+import styles from "../../styles/Signin.module.css";
 
 const Login = () => {
   // load states from redux
@@ -15,12 +17,12 @@ const Login = () => {
 
   // create login form state
   const [formData, setFormData] = useState({
-    username: "",
+    email: "",
     password: "",
   });
 
   // destructure login data from state
-  const { username, password } = formData;
+  const { email, password } = formData;
 
   useEffect(() => {
     dispatch(reset_register_success());
@@ -39,13 +41,13 @@ const Login = () => {
   // create form submit handler function
   const onSubmit = (e) => {
     e.preventDefault();
-    if (!username || !password) {
+    if (!email || !password) {
       toast.error("Please provide all fields");
       return;
     } else {
       dispatch(login(formData));
       setFormData({
-        username: "",
+        email: "",
         password: "",
       });
     }
@@ -57,40 +59,111 @@ const Login = () => {
   }
 
   return (
-    <div>
-      <h2>Welcome to the login page</h2>
-
-      <form onSubmit={onSubmit}>
-        <div className="form_group">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            value={username}
-            name="username"
-            onChange={onChange}
+    <div className={styles.container}>
+      <div className={styles.A}>
+        <div className={styles.top}>
+          <Image
+            src="/assets/Frame.svg"
+            alt="top-line"
+            width={630}
+            height={850}
           />
-        </div>
-        <div className="form_group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            value={password}
-            name="password"
-            onChange={onChange}
-          />
-          <Link href="/account/forgotPassword">Forgot Password?</Link>
-        </div>
+          <div className={styles.logo_text}>
+            <Image
+              className={styles.img}
+              src="/assets/auth_wiki.svg"
+              alt="logo"
+              width={41}
+              height={64}
+            />
 
-        <button type="submit" className="btn">
-          {loading ? (
-            <ThreeDots color="#FFF" height={40} width={40} />
-          ) : (
-            "Login"
-          )}
-        </button>
+            <h2>Welcome back</h2>
+            <p>
+              Sign in to get access to all pages, download codes and contribute.
+            </p>
+          </div>
+        </div>
+      </div>
 
-        {/* Register? */}
-      </form>
+      <div className={styles.B}>
+        <header className={styles.links}>
+          {/* Signin Link */}
+          <div className={`${styles.sign_link} ${styles.signin}`}>
+            <h3>
+              <Link href="/account/login">Sign In</Link>
+            </h3>
+            <div className={styles.underline}></div>
+          </div>
+
+          {/* signup link */}
+          <div className={`${styles.sign_link} ${styles.signup}`}>
+            <h3>
+              <Link href="/account/register">Sign Up</Link>
+            </h3>
+            <div className={styles.underline}></div>
+          </div>
+        </header>
+
+        <form onSubmit={onSubmit}>
+          <div className="form_group">
+            <label className="label" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              name="email"
+              onChange={onChange}
+              placeholder="Please enter your email"
+            />
+          </div>
+          <div className="form_group">
+            <label className="label" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              name="password"
+              onChange={onChange}
+              placeholder="Please enter your password"
+            />
+          </div>
+          <div className={styles.checkbox_link}>
+            <div className={styles.checkbox}>
+              <input type="checkbox" name="remember-me" id="remember-me" />
+              <label htmlFor="remember-me">
+                <p>Remember Me</p>
+              </label>
+            </div>
+            <div className={styles.forgot_link}>
+              <Link href="/account/forgotPassword">Forgot Password?</Link>
+            </div>
+          </div>
+
+          <button type="submit" className={styles.signinbtn}>
+            {loading ? (
+              <ThreeDots color="#FFF" height={40} width={40} />
+            ) : (
+              "Sign In"
+            )}
+          </button>
+          <h5 className={styles.or_text}>OR</h5>
+          <div className="github_btn">
+            <button className="github-accnt">
+              <Image
+                src="/assets/Github.svg"
+                alt="github_logo"
+                width={24}
+                height={24}
+              />
+              <h6 className={styles.git_text}>Continue with Github</h6>
+            </button>
+          </div>
+
+          {/* Register? */}
+        </form>
+      </div>
     </div>
   );
 };
