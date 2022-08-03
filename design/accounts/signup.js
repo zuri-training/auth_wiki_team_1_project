e6f54206eeded439ca1b7cd53f6dc6cdba423e23
Remember <kelvin6bolou@gmail.com>
2022-08-03 20:40:33 +0100

    update: add fontawesome eye icon to password input box style: add style for eye icon feat: add event listener to show/hide password

--- design/accounts/signup.js
+++ design/accounts/signup.js
@@ -2,6 +2,23 @@
 const fname = document.getElementById('fullname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const icon = document.getElementById('eye');
+
+// event listener to toggle password type with eye icon
+icon.addEventListener('click', function () {
+  passwordType();
+  icon.classList.toggle('fa-eye-slash');
+});
+
+// function to toggle password and confirm password type to text
+function passwordType() {
+  // for password
+  if (password.type === 'password') {
+    password.type = 'text';
+  } else {
+    password.type = 'password';
+  }
+}
 
 // function to validate user input
 function validateInput() {
@@ -38,13 +55,13 @@ function validateInput() {
   //   password
   if (password.value.trim() === '') {
     // invoking error function for when user leaves input blank
-    onError(password, 'Password cannot be empty');
+    onErrorPassword(password, 'Password cannot be empty');
     // change the placeholder display text
     let changeText = document.getElementById('password');
     changeText.setAttribute('placeholder', '');
   } else {
     // invoking success function for when user inputs right info
-    onSuccess(password);
+    onSuccessPassword(password);
   }
 }
 
@@ -81,6 +98,29 @@ function onError(input, emessage) {
   //   error message when user input is not correct
   message.innerText = emessage;
   //   add error class from 'small' element when input is not correct
+  parent.classList.add('error');
+}
+
+// succes function for password
+function onSuccessPassword(input) {
+  // get parent container holding the input
+  let parent = input.parentElement.parentElement;
+  //   get element where error message would be displayed
+  let message = parent.querySelector('small');
+  //   hide the visibility when user input is correct
+  message.style.visibility = 'hidden';
+  //   remove error message when user input is correct
+  message.innerText = '';
+  //   remove error class from 'small' element when input is correct
+  parent.classList.remove('error');
+}
+
+// error function for password
+function onErrorPassword(input, emessage) {
+  let parent = input.parentElement.parentElement;
+  let message = parent.querySelector('small');
+  message.style.visibility = 'visible';
+  message.innerText = emessage;
   parent.classList.add('error');
 }
 
