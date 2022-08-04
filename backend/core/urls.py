1b5e4d3b6b523fd2ee8b442897682cc00d38398a
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-08-04 20:14:16 +0100

    update views

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api/auth/", include("auth.urls", namespace="auth")),
-    path("wiki/", include("wiki.urls", namespace="wiki")),
+    path("api/wiki/", include("wiki.urls", namespace="wiki")),
 ]
