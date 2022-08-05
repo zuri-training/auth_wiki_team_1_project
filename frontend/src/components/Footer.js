f5677ee6cc8acf03cb1f776f0cece4a327f2858e
0papi <evanskwofie67@gmail.com>
2022-08-05 08:50:01 +0000

    fixed some styling issues

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,93 +6,95 @@ import styles from "../styles/Footer.module.css";
 const Footer = () => {
   const getDate = new Date().getFullYear();
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footerWrapper}>
       {/* footer top */}
-      <div className={styles.footer_top}>
-        <div className={styles.footer_top_info}>
-          <Image
-            src="/assets/auth_wiki.svg"
-            alt="auth wiki logo"
-            width={40}
-            height={54}
-          />
+      <div className={`container ${styles.footer}`}>
+        <div className={styles.footer_top}>
+          <div className={styles.footer_top_info}>
+            <Image
+              src="/assets/auth_wiki.svg"
+              alt="auth wiki logo"
+              width={40}
+              height={54}
+            />
 
-          <div>
-            <p>Information</p>
-            <ul>
+            <div>
+              <p>Information</p>
+              <ul>
+                <li>
+                  <Link href="/faq">
+                    <a>FAQs</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/about">
+                    <a>About Us</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/documenation">
+                    <a>Documentation</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* contact */}
+          <div className={styles.footer_top_contact}>
+            <p>Contact Us</p>
+            <ul className="flex">
               <li>
-                <Link href="/faq">
-                  <a>FAQs</a>
+                <Link href="#">
+                  <a>
+                    <FaTwitter />
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link href="/about">
-                  <a>About Us</a>
+                <Link href="#">
+                  <a>
+                    <FaFacebookF />
+                  </a>
                 </Link>
               </li>
               <li>
-                <Link href="/documenation">
-                  <a>Documentation</a>
+                <Link href="#">
+                  <a>
+                    <FaInstagram />
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a>
+                    <FaGithub />
+                  </a>
                 </Link>
               </li>
             </ul>
           </div>
-        </div>
-        {/* contact */}
-        <div className={styles.footer_top_contact}>
-          <p>Contact Us</p>
-          <ul className="flex">
-            <li>
-              <Link href="#">
-                <a>
-                  <FaTwitter />
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>
-                  <FaFacebookF />
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>
-                  <FaInstagram />
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <a>
-                  <FaGithub />
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        {/* newsletter */}
-        <div className={styles.footer_top_subscribe}>
-          <p>Never miss a story!</p>
-          <span>Subscribe to our newsletter.</span>
-          <div>
-            <form>
-              <input type="email" placeholder="Email Address" />
-              <button type="submit">Submit</button>
-            </form>
+          {/* newsletter */}
+          <div className={styles.footer_top_subscribe}>
+            <p>Never miss a story!</p>
+            <span>Subscribe to our newsletter.</span>
+            <div>
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button type="submit">Submit</button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
 
-      {/* footer bottom */}
-      <div>
-        <div className="line">
-          <hr></hr>
+        {/* footer bottom */}
+        <div>
+          <div className="line">
+            <hr></hr>
+          </div>
+          <span className="foooter_bottom_text">
+            &copy; Copyright {getDate}, All Rights Reserved - Zuri Team
+          </span>
         </div>
-        <span className="foooter_bottom_text">
-          &copy; Copyright {getDate}, All Rights Reserved - Zuri Team
-        </span>
       </div>
     </footer>
   );
