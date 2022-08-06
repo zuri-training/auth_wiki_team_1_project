92fe1a2ce7b8933102d31a11d7a68723d4bdfece
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-06 01:50:56 +0100

    Removed the github login feature

--- frontend/src/pages/account/login.js
+++ frontend/src/pages/account/login.js
@@ -165,7 +165,7 @@ const Login = () => {
               "Sign In"
             )}
           </button>
-          <h5 className={styles.or_text}>OR</h5>
+          {/* <h5 className={styles.or_text}>OR</h5>
           <div className="github_btn">
             <button className="github-accnt">
               <Image
@@ -176,7 +176,7 @@ const Login = () => {
               />
               <h6 className={styles.git_text}>Continue with Github</h6>
             </button>
-          </div>
+          </div> */}
 
           {/* Register? */}
         </form>
