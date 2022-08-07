a751a10e41b77aaf648b61d90fe2155d2f835768
Ayomide0123 <oyetimehin31@gmail.com>
2022-08-07 17:37:41 +0100

    CRUD_Reaction_Model
    CRUD Operation for the reaction model

--- backend/wiki/urls.py
+++ backend/wiki/urls.py
@@ -6,8 +6,9 @@ from rest_framework import routers
 app_name = "wiki"
 
 router = routers.DefaultRouter()
-router.register(r'posts', views.PostViewSet, basename='posts')
-router.register(r'comments', views.CommentViewSet, basename='comments')
-router.register(r'files', views.FileViewSet, basename='files')
+router.register(r"posts", views.PostViewSet, basename="posts")
+router.register(r"comments", views.CommentViewSet, basename="comments")
+router.register(r"files", views.FileViewSet, basename="files")
+router.register(r"files", views.ReactionViewSet, basename="reaction")
 
 urlpatterns = router.urls
