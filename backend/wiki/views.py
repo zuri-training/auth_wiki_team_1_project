6c5516a9833f928e42bdce330dbe3a41ccf0341b
TimmyKwesi <timmysteinx@gmail.com>
2022-08-07 15:26:19 +0000

    Search Functionality added

--- backend/wiki/views.py
+++ backend/wiki/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Comment, Post, File
 from .serializers import PostSerializer, FileSerializer, CommentSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Create your views here.
 
@@ -9,6 +10,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('title', 'description', 'user')
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
@@ -19,4 +22,6 @@ class FileViewSet(viewsets.ModelViewSet):
     queryset  = File.objects.all()
     serializer_class = FileSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('name', 'post')
+    
