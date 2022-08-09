e7fe2517103f03e7b062f3df14d8c705ed80208e
Mudassir Adili Ahmad <mudassiradili4u@gmail.com>
2022-08-09 08:57:59 +0100

    Update urls.py

--- backend/wiki/urls.py
+++ backend/wiki/urls.py
@@ -9,6 +9,6 @@ router = routers.DefaultRouter()
 router.register(r"posts", views.PostViewSet, basename="posts")
 router.register(r"comments", views.CommentViewSet, basename="comments")
 router.register(r"files", views.FileViewSet, basename="files")
-router.register(r"files", views.ReactionViewSet, basename="reaction")
+router.register(r"reactions", views.ReactionViewSet, basename="reaction")
 
 urlpatterns = router.urls
