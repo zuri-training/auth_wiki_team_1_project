bcc95cb9ddee88c91629772c7d4a9fe756714662
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-11 14:25:27 +0100

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,20 +2,30 @@ import Layout from "../components/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "../styles/globals.css";
+import styles from "../styles/Loader.module.css";
 
 import { Provider } from "react-redux";
 import { useStore } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
   return (
-    <Provider store={store}>
-      <Layout>
+    <Provider store={store.store}>
+      <PersistGate loading={<Loader />} persistor={store.persistor}>
         <Component {...pageProps} />
         <ToastContainer />
-      </Layout>
+      </PersistGate>
     </Provider>
   );
 }
 
 export default MyApp;
+
+function Loader() {
+  return (
+    <div className={styles.loader_wrapper}>
+      <div className={styles.loader} />
+    </div>
+  );
+}
