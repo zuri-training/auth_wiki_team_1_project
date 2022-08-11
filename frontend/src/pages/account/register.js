73e9be81cdd79505fc67e7bd8445ed4dd806b8f3
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-11 23:11:10 +0100

    Update register.js

--- frontend/src/pages/account/register.js
+++ frontend/src/pages/account/register.js
@@ -9,6 +9,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { register } from "../../actions/account";
 import { PrimaryButton } from "../../components/utils/Buttons";
 import styles from "../../styles/Signup.module.css";
+import AuthLayout from "../../components/Layouts/AuthLayout";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -81,172 +82,174 @@ const Register = () => {
   };
 
   return (
-    <div className={`container ${styles.signUpWrapper}`}>
-      <div className={styles.A}>
-        <div className={styles.top}>
-          <Image
-            src="/assets/topellipse.svg"
-            alt="top-line"
-            width={580}
-            height={450}
-          />
+    <AuthLayout>
+      <div className={`container ${styles.signUpWrapper}`}>
+        <div className={styles.A}>
+          <div className={styles.top}>
+            <Image
+              src="/assets/topellipse.svg"
+              alt="top-line"
+              width={580}
+              height={450}
+            />
+          </div>
+          <div className={styles.logo_text}>
+            <Image
+              className={styles.img}
+              src="/assets/auth_wiki.svg"
+              alt="logo"
+              width={41}
+              height={64}
+            />
+
+            <h2>Welcome</h2>
+            <p>
+              Get access to various Authentication codes, download codes and
+              enjoy all benefits as a user.
+            </p>
+          </div>
+          <div className={styles.bottom}>
+            <Image
+              src="/assets/bottom ellipse.svg"
+              alt="top-line"
+              width={330}
+              height={450}
+            />
+          </div>
         </div>
-        <div className={styles.logo_text}>
-          <Image
-            className={styles.img}
-            src="/assets/auth_wiki.svg"
-            alt="logo"
-            width={41}
-            height={64}
-          />
 
-          <h2>Welcome</h2>
-          <p>
-            Get access to various Authentication codes, download codes and enjoy
-            all benefits as a user.
-          </p>
-        </div>
-        <div className={styles.bottom}>
-          <Image
-            src="/assets/bottom ellipse.svg"
-            alt="top-line"
-            width={330}
-            height={450}
-          />
-        </div>
-      </div>
+        <div className={styles.B}>
+          <header className={styles.links}>
+            {/* Signin Link */}
+            <div className={`${styles.sign_link} ${styles.signin}`}>
+              <h3>
+                <Link href="/account/login">Sign In</Link>
+              </h3>
+              <div className={styles.underline}></div>
+            </div>
 
-      <div className={styles.B}>
-        <header className={styles.links}>
-          {/* Signin Link */}
-          <div className={`${styles.sign_link} ${styles.signin}`}>
-            <h3>
-              <Link href="/account/login">Sign In</Link>
-            </h3>
-            <div className={styles.underline}></div>
-          </div>
-
-          {/* signup link */}
-          <div className={`${styles.sign_link} ${styles.signup}`}>
-            <h3>
-              <Link href="/account/register">Sign Up</Link>
-            </h3>
-            <div className={styles.underline}></div>
-          </div>
-        </header>
-        <form onSubmit={onSubmit}>
-          <div className="form_group">
-            <label htmlFor="username" className={styles.label}>
-              Username
-            </label>
-            <input
-              type="text"
-              value={username}
-              name="username"
-              onChange={onChange}
-              placeholder="Please enter your username"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.form_group}>
-            <label htmlFor="first_name" className={styles.label}>
-              First Name
-            </label>
-            <input
-              type="text"
-              value={first_name}
-              name="first_name"
-              onChange={onChange}
-              placeholder="Please enter your first name"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.form_group}>
-            <label htmlFor="last_name" className={styles.label}>
-              Last Name
-            </label>
-            <input
-              type="text"
-              value={last_name}
-              name="last_name"
-              onChange={onChange}
-              placeholder="Please enter your last name"
-              className={styles.input}
-            />
-          </div>
-
-          <div className={styles.form_group}>
-            <label htmlFor="email" className={styles.label}>
-              Email
-            </label>
-            <input
-              type="email"
-              value={email}
-              name="email"
-              onChange={onChange}
-              placeholder="Please enter your email"
-              className={styles.input}
-            />
-          </div>
-
-          <div className={styles.form_group}>
-            <label htmlFor="password1" className={styles.label}>
-              Password
-            </label>
-            <input
-              type={passwordShown ? "text" : "password"}
-              value={password1}
-              name="password1"
-              onChange={onChange}
-              placeholder="Please enter your password"
-              className={styles.input}
-            />
-            <div className="eye_icon">
-              <Image
-                onClick={togglePassword}
-                src="/assets/eye.svg"
-                alt="eye-icon"
-                width={24}
-                height={24}
+            {/* signup link */}
+            <div className={`${styles.sign_link} ${styles.signup}`}>
+              <h3>
+                <Link href="/account/register">Sign Up</Link>
+              </h3>
+              <div className={styles.underline}></div>
+            </div>
+          </header>
+          <form onSubmit={onSubmit}>
+            <div className="form_group">
+              <label htmlFor="username" className={styles.label}>
+                Username
+              </label>
+              <input
+                type="text"
+                value={username}
+                name="username"
+                onChange={onChange}
+                placeholder="Please enter your username"
+                className={styles.input}
               />
             </div>
-          </div>
-          <div className={styles.form_group}>
-            <label htmlFor="password2" className={styles.label}>
-              Confirm Password
-            </label>
-            <input
-              type={passwordShown ? "text" : "password"}
-              value={password2}
-              name="password2"
-              onChange={onChange}
-              placeholder="Please confirm your password"
-              className={styles.input}
-            />
-          </div>
-
-          <PrimaryButton type="submit" className={styles.signupbtn}>
-            {loading ? (
-              <ThreeDots color="#FFF" height={40} width={40} />
-            ) : (
-              "Sign Up"
-            )}
-          </PrimaryButton>
-          {/* <h5 className={styles.or_text}>OR</h5>
-          <div className="github_btn">
-            <button className="github-accnt">
-              <Image
-                src="/assets/Github.svg"
-                alt="github_logo"
-                width={24}
-                height={24}
+            <div className={styles.form_group}>
+              <label htmlFor="first_name" className={styles.label}>
+                First Name
+              </label>
+              <input
+                type="text"
+                value={first_name}
+                name="first_name"
+                onChange={onChange}
+                placeholder="Please enter your first name"
+                className={styles.input}
               />
-              <h6 className={styles.git_text}>Continue with Github</h6>
-            </button>
-          </div> */}
-        </form>
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="last_name" className={styles.label}>
+                Last Name
+              </label>
+              <input
+                type="text"
+                value={last_name}
+                name="last_name"
+                onChange={onChange}
+                placeholder="Please enter your last name"
+                className={styles.input}
+              />
+            </div>
+
+            <div className={styles.form_group}>
+              <label htmlFor="email" className={styles.label}>
+                Email
+              </label>
+              <input
+                type="email"
+                value={email}
+                name="email"
+                onChange={onChange}
+                placeholder="Please enter your email"
+                className={styles.input}
+              />
+            </div>
+
+            <div className={styles.form_group}>
+              <label htmlFor="password1" className={styles.label}>
+                Password
+              </label>
+              <input
+                type={passwordShown ? "text" : "password"}
+                value={password1}
+                name="password1"
+                onChange={onChange}
+                placeholder="Please enter your password"
+                className={styles.input}
+              />
+              <div className="eye_icon">
+                <Image
+                  onClick={togglePassword}
+                  src="/assets/eye.svg"
+                  alt="eye-icon"
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </div>
+            <div className={styles.form_group}>
+              <label htmlFor="password2" className={styles.label}>
+                Confirm Password
+              </label>
+              <input
+                type={passwordShown ? "text" : "password"}
+                value={password2}
+                name="password2"
+                onChange={onChange}
+                placeholder="Please confirm your password"
+                className={styles.input}
+              />
+            </div>
+
+            <PrimaryButton type="submit" className={styles.signupbtn}>
+              {loading ? (
+                <ThreeDots color="#FFF" height={40} width={40} />
+              ) : (
+                "Sign Up"
+              )}
+            </PrimaryButton>
+            {/* <h5 className={styles.or_text}>OR</h5>
+            <div className="github_btn">
+              <button className="github-accnt">
+                <Image
+                  src="/assets/Github.svg"
+                  alt="github_logo"
+                  width={24}
+                  height={24}
+                />
+                <h6 className={styles.git_text}>Continue with Github</h6>
+              </button>
+            </div> */}
+          </form>
+        </div>
       </div>
-    </div>
+    </AuthLayout>
   );
 };
 
