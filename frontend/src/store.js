a4fd220a03f3c1345f5ae7940e853eb0168a1f29
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-11 14:25:44 +0100

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,40 +3,54 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 import reducers from "./reducers";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, reducers);
 let store;
 
 function initStore(initialState) {
-    return createStore(
-        reducers,
-        initialState,
-        composeWithDevTools(applyMiddleware(thunkMiddleware))
-    );
+  return createStore(
+    persistedReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunkMiddleware))
+  );
 }
 
 export const initializeStore = (preloadedState) => {
-    let _store = store ?? initStore(preloadedState);
+  let _store = store ?? initStore(preloadedState);
 
-    // After navigating to a page with an initial Redux state, merge that state
-    // with the current state in the store, and create a new store
-    if (preloadedState && store) {
-        _store = initStore({
-            ...store.getState(),
-            ...preloadedState,
-        });
-        // Reset the current store
-        store = undefined;
-    }
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    _store = initStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
+  }
 
-    // For SSG and SSR always create a new store
-    if (typeof window === "undefined") return _store;
-    // Create the store once in the client
-    if (!store) store = _store;
+  // For SSG and SSR always create a new store
+  if (typeof window === "undefined") return _store;
+  // Create the store once in the client
+  if (!store) store = _store;
 
-    return _store;
+  //   I think we should use PersistStore here, okay
+  // i no sabi oo, i just dey try lmao
+
+  return _store;
 };
 
 export function useStore(initialState) {
-    const store = useMemo(() => initializeStore(initialState), [ initialState ]);
-    return store;
+  const store = useMemo(() => initializeStore(initialState), [initialState]);
+  const persistor = useMemo(
+    () => persistStore(initializeStore(initialState)),
+    [initialState]
+  );
+  return { store, persistor };
 }
