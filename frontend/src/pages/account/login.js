5cf230aeeea2424c21c58d969bd82b2a7a191c83
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-11 23:11:05 +0100

    Update login.js

--- frontend/src/pages/account/login.js
+++ frontend/src/pages/account/login.js
@@ -7,6 +7,7 @@ import { login, reset_register_success } from "../../actions/account";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
 import styles from "../../styles/Signin.module.css";
+import AuthLayout from "../../components/Layouts/AuthLayout";
 
 const Login = () => {
   // load states from redux
@@ -55,7 +56,7 @@ const Login = () => {
 
   // redirect to dashboard page if user is logged in
   if (isLoggedIn) {
-    router.push("/dashboard");
+    router.push("/");
   }
 
   // password toggle visibility
@@ -65,123 +66,126 @@ const Login = () => {
   };
 
   return (
-    <div className={`container ${styles.loginWrapper}`}>
-      <div className={styles.A}>
-        <div className={styles.top}>
-          <Image
-            src="/assets/Frame.svg"
-            alt="top-line"
-            width={630}
-            height={850}
-          />
-          <div className={styles.logo_text}>
+    <AuthLayout>
+      <div className={`container ${styles.loginWrapper}`}>
+        <div className={styles.A}>
+          <div className={styles.top}>
             <Image
-              className={styles.img}
-              src="/assets/auth_wiki.svg"
-              alt="logo"
-              width={41}
-              height={64}
+              src="/assets/Frame.svg"
+              alt="top-line"
+              width={630}
+              height={850}
             />
+            <div className={styles.logo_text}>
+              <Image
+                className={styles.img}
+                src="/assets/auth_wiki.svg"
+                alt="logo"
+                width={41}
+                height={64}
+              />
 
-            <h2>Welcome back</h2>
-            <p>
-              Sign in to get access to all pages, download codes and contribute.
-            </p>
+              <h2>Welcome back</h2>
+              <p>
+                Sign in to get access to all pages, download codes and
+                contribute.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.B}>
-        <header className={styles.links}>
-          {/* Signin Link */}
-          <div className={`${styles.sign_link} ${styles.signin}`}>
-            <h3>
-              <Link href="/account/login">Sign In</Link>
-            </h3>
-            <div className={styles.underline}></div>
-          </div>
-
-          {/* signup link */}
-          <div className={`${styles.sign_link} ${styles.signup}`}>
-            <h3>
-              <Link href="/account/register">Sign Up</Link>
-            </h3>
-            <div className={styles.underline}></div>
-          </div>
-        </header>
-
-        <form onSubmit={onSubmit}>
-          <div className="form_group">
-            <label className="label" htmlFor="username">
-              Username
-            </label>
-            <input
-              type="username"
-              value={username}
-              name="username"
-              onChange={onChange}
-              placeholder="Please enter your username"
-            />
-          </div>
-          <div className="form_group">
-            <label className="label" htmlFor="password">
-              Password
-            </label>
-            <input
-              type={passwordShown ? "text" : "password"}
-              value={password}
-              name="password"
-              onChange={onChange}
-              placeholder="Please enter your password"
-            />
-
-            <div className="eye_icon">
-              <Image
-                onClick={togglePassword}
-                src="/assets/eye.svg"
-                alt="eye-icon"
-                width={24}
-                height={24}
-                // className={styles.eye_icon}
-              />
+        <div className={styles.B}>
+          <header className={styles.links}>
+            {/* Signin Link */}
+            <div className={`${styles.sign_link} ${styles.signin}`}>
+              <h3>
+                <Link href="/account/login">Sign In</Link>
+              </h3>
+              <div className={styles.underline}></div>
             </div>
-          </div>
-          <div className={styles.checkbox_link}>
-            <div className={styles.checkbox}>
-              <input type="checkbox" name="remember-me" id="remember-me" />
-              <label htmlFor="remember-me">
-                <p>Remember Me</p>
+
+            {/* signup link */}
+            <div className={`${styles.sign_link} ${styles.signup}`}>
+              <h3>
+                <Link href="/account/register">Sign Up</Link>
+              </h3>
+              <div className={styles.underline}></div>
+            </div>
+          </header>
+
+          <form onSubmit={onSubmit}>
+            <div className="form_group">
+              <label className="label" htmlFor="username">
+                Username
               </label>
-            </div>
-            <div className={styles.forgot_link}>
-              <Link href="/account/forgotPassword">Forgot Password?</Link>
-            </div>
-          </div>
-
-          <button type="submit" className={styles.signinbtn}>
-            {loading ? (
-              <ThreeDots color="#FFF" height={40} width={40} />
-            ) : (
-              "Sign In"
-            )}
-          </button>
-          {/* <h5 className={styles.or_text}>OR</h5>
-          <div className="github_btn">
-            <button className="github-accnt">
-              <Image
-                src="/assets/Github.svg"
-                alt="github_logo"
-                width={24}
-                height={24}
+              <input
+                type="username"
+                value={username}
+                name="username"
+                onChange={onChange}
+                placeholder="Please enter your username"
               />
-              <h6 className={styles.git_text}>Continue with Github</h6>
-            </button>
-          </div> */}
+            </div>
+            <div className="form_group">
+              <label className="label" htmlFor="password">
+                Password
+              </label>
+              <input
+                type={passwordShown ? "text" : "password"}
+                value={password}
+                name="password"
+                onChange={onChange}
+                placeholder="Please enter your password"
+              />
 
-          {/* Register? */}
-        </form>
+              <div className="eye_icon">
+                <Image
+                  onClick={togglePassword}
+                  src="/assets/eye.svg"
+                  alt="eye-icon"
+                  width={24}
+                  height={24}
+                  // className={styles.eye_icon}
+                />
+              </div>
+            </div>
+            <div className={styles.checkbox_link}>
+              <div className={styles.checkbox}>
+                <input type="checkbox" name="remember-me" id="remember-me" />
+                <label htmlFor="remember-me">
+                  <p>Remember Me</p>
+                </label>
+              </div>
+              <div className={styles.forgot_link}>
+                <Link href="/account/forgotPassword">Forgot Password?</Link>
+              </div>
+            </div>
+
+            <button type="submit" className={styles.signinbtn}>
+              {loading ? (
+                <ThreeDots color="#FFF" height={40} width={40} />
+              ) : (
+                "Sign In"
+              )}
+            </button>
+            {/* <h5 className={styles.or_text}>OR</h5>
+            <div className="github_btn">
+              <button className="github-accnt">
+                <Image
+                  src="/assets/Github.svg"
+                  alt="github_logo"
+                  width={24}
+                  height={24}
+                />
+                <h6 className={styles.git_text}>Continue with Github</h6>
+              </button>
+            </div> */}
+
+            {/* Register? */}
+          </form>
+        </div>
       </div>
-    </div>
+    </AuthLayout>
   );
 };
 
