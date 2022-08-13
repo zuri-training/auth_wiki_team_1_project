63a6890ae2c10112328d38161f6628b63076b133
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-08-13 09:01:46 +0100

    Update support.js

--- frontend/src/pages/support/support.js
+++ frontend/src/pages/support/support.js
@@ -1,48 +1,52 @@
+import AppLayout from "../../components/Layouts/AppLayout";
 import styles from "../../styles/Support.module.css";
-
 
 const support = () => {
   return (
-    <>
-  <div className={styles.cnt}>
-    <div className={styles.wrapper}>
-      <h1>Get Support</h1>
-      <p>We are here for you, let's help you solve any technical issues</p>
-    </div>
-  </div>
-  <div>
-    <form>
-      <div className={styles.frame1}>
-        <div className={styles.fname}>
-          <label>First Name</label>
-          <input type="text" placeholder="Please enter your name" />
+    <AppLayout>
+      <>
+        <div className={styles.cnt}>
+          <div className={styles.wrapper}>
+            <h1>Get Support</h1>
+            <p>
+              We are here for you, let's help you solve any technical issues
+            </p>
+          </div>
         </div>
-        <div className={styles.lname}>
-          <label>Last Name</label>
-          <input type="text" placeholder="Please enter your last name" />
+        <div>
+          <form>
+            <div className={styles.frame1}>
+              <div className={styles.fname}>
+                <label>First Name</label>
+                <input type="text" placeholder="Please enter your name" />
+              </div>
+              <div className={styles.lname}>
+                <label>Last Name</label>
+                <input type="text" placeholder="Please enter your last name" />
+              </div>
+            </div>
+            <div className={styles.frame2}>
+              <div className={styles.em}>
+                <label>Email</label>
+                <input type="email" placeholder="Please enter your email" />
+              </div>
+              <div className={styles.phnum}>
+                <label>Phone Number</label>
+                <input type="text" placeholder="Please enter your number" />
+              </div>
+            </div>
+            <div className={styles.iptfield}>
+              <p>How can we help you?</p>
+              <textarea defaultValue={""} />
+            </div>
+            <div className={styles.btncontainer}>
+              <button className={styles.btn}>Submit</button>
+            </div>
+          </form>
         </div>
-      </div>
-      <div className={styles.frame2}>
-        <div className={styles.em}>
-          <label>Email</label>
-          <input type="email" placeholder="Please enter your email" />
-        </div>
-        <div className={styles.phnum}>
-          <label>Phone Number</label>
-          <input type="text" placeholder="Please enter your number" />
-        </div>
-      </div>
-      <div className={styles.iptfield}>
-        <p>How can we help you?</p>
-        <textarea defaultValue={""} />
-      </div>
-      <div className={styles.btncontainer}>
-        <button className={styles.btn}>Submit</button>
-      </div>
-    </form>
-  </div>
-</>
-  )
-}
+      </>
+    </AppLayout>
+  );
+};
 
-export default support
+export default support;
